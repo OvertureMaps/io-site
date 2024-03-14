908100a8a27bd20e8f3b9f00f5ff7137dd199f58
Benjamin Clark <clarkben@fb.com>
2024-03-14 15:15:12 -0400

    Extract site code into header and footer functional components.

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,24 +1,14 @@
-import { useState } from 'react'
-import overtureLogo from '/omf_logo_transparent.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>OvertureMaps.io</h1>
-      <div>
-        <a href="https://overturemaps.org" target="_blank">
-          <img src={overtureLogo} className="logo overture" alt="Overture Maps logo" />
-        </a>
-      </div>
-      <p className="read-the-docs">
-        Head on over to <a href="https://docs.overturemaps.org/how-to" target="_blank"> the documentation</a> to learn more
-      </p>
+      <Header />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
