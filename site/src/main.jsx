04404fee5dfeb9f869d493997fcdbc021344a841
Benjamin Clark <clarkben@meta.com>
2024-04-08 11:11:27 -0400

    Add geoarrow module, and example code to build with it and instantiate a new Parquet File.

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import initWasm, {ParquetFile} from "@geoarrow/geoarrow-wasm/esm/index.js";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+await initWasm();
+
+ let exampleFile = await new ParquetFile('example.parquet');
+ const wasmTable = await exampleFile.readRowGroups([0]);
+
+ console.log(wasmTable); 
