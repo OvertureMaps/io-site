efc49765f24439c4d2a49cc27f1f68e95647d0bd
Benjamin Clark <clarkben@meta.com>
2024-04-08 15:27:18 -0400

    Switch to loading remote parquet file from overture's nov 14 2023 bucket

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -13,7 +13,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 await initWasm();
 
- let exampleFile = await new ParquetFile('example.parquet');
- const wasmTable = await exampleFile.readRowGroups([0]);
+const url =
+"https://overturemaps-us-west-2.s3.amazonaws.com/release/2023-11-14-alpha.0/theme=transportation/type=segment/part-00158-6cb89013-4ec2-4b94-8e4b-8e27c7d30865.c000.zstd.parquet";
+// let exampleFile = await new ParquetFile('./example.parquet');
+let exampleFile = await new ParquetFile(url);
+// const wasmTable = await exampleFile.readRowGroups([0]);
 
  console.log(wasmTable); 
