16ee2639fbb2bd403407f8ca006397aa5b21bebe
Benjamin Clark <clarkben@meta.com>
2024-04-12 16:18:06 -0400

    Use static copy plugin to fetch the correct esm file and put it in a virtual 'assets' folder.

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import initWasm, {ParquetFile} from "@geoarrow/geoarrow-wasm/esm/index.js";
-import index_bg from '/index_bg.wasm?url'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,8 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-console.log(`Attempting to load parquet-wasm from url '${index_bg}'`)
-await initWasm(index_bg);
+await initWasm();
 
 const url =
 "https://overturemaps-us-west-2.s3.amazonaws.com/release/2023-11-14-alpha.0/theme=transportation/type=segment/part-00158-6cb89013-4ec2-4b94-8e4b-8e27c7d30865.c000.zstd.parquet";
--- site/vite.config.js
+++ site/vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/@geoarrow/geoarrow-wasm/esm/index_bg.wasm',
+          dest: 'assets'
+        }
+      ]
+    })
+  ],
   build: {
     target: 'esnext',
-  },
-  optimizeDeps: {
-    exclude: ["@rollup/browser"],
-  },
+  }
 })
