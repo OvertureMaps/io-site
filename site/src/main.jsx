4029bd899aa40386d98b20c1d376ed2a519a5763
Benjamin Clark <clarkben@meta.com>
2024-04-12 18:29:01 -0400

    Bump the file we're using to do the queries to something more recent (and better partitioned).

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -13,12 +13,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 await initWasm();
 
 const url =
-"https://overturemaps-us-west-2.s3.amazonaws.com/release/2023-11-14-alpha.0/theme=transportation/type=segment/part-00158-6cb89013-4ec2-4b94-8e4b-8e27c7d30865.c000.zstd.parquet";
+"https://overturemaps-us-west-2.s3.amazonaws.com/release/2024-03-12-alpha.0/theme=transportation/type=segment/part-00000-26412a13-1721-4841-b165-ec480de266f5-c000.zstd.parquet";
 let exampleFile = await new ParquetFile(url);
  const wasmTable = await exampleFile.readRowGroups([0], {
   batch_size: 1024,
-  limit: 100,
-  offset: 0
+  limit: 100000,
+  offset: 0, 
+  bbox: [-71.068, 42.353, -71.058, 42.363]
 });
 
  console.log(wasmTable); 
