280646d1f534a9e54487f6cab483423db241955b
Benjamin Clark <clarkben@meta.com>
2024-04-12 13:57:47 -0400

    Add vite plugin for wasm.

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import wasm from 'vite-plugin-wasm'
 //import wasmPack from 'vite-plugin-wasm-pack'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm()],
   build: {
     target: 'esnext',
   }
