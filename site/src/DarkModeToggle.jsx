b29ebf3d9da44954a0900cc0e8568c36d44c60e1
Benjamin Clark <clarkben@meta.com>
2024-04-30 19:58:28 -0400

    Dark mode kinda works!

--- site/src/DarkModeToggle.jsx
+++ site/src/DarkModeToggle.jsx
@@ -1,26 +1,27 @@
 import { setTheme } from './themeUtils';
 import PropTypes from 'prop-types';
 
-export default function DarkModeToggle ({mode, setMode}) {
+export default function DarkModeToggle({ mode, setMode }) {
 
-  const toggleDarkMode = () => {
+    const toggleDarkMode = () => {
 
-    if (mode === 'theme-dark') {
-        setTheme('theme-light', setMode);
-    } else {
-        setTheme('theme-dark', setMode);
-    }
-  };
+        if (mode === 'theme-dark') {
+            setTheme('theme-light', setMode);
+        } else {
+            setTheme('theme-dark', setMode);
+        }
+    };
 
-  return (
-    <div>
-<input role="switch" type="checkbox" id="toggle" className="toggle--checkbox" onClick={toggleDarkMode} checked={mode === 'theme-dark'} readOnly />
-            <img src={mode === 'theme-light' ? '/lightmode.svg' : '/darkmode.svg'}/>
-    </div>
-  );
+    return (
+        <div>
+            <button id="toggle" className="toggle--checkbox" onClick={toggleDarkMode} readOnly>
+                <img src={mode === 'theme-light' ? '/lightmode.svg' : '/darkmode.svg'} />
+            </button>
+        </div>
+    );
 }
 
 DarkModeToggle.propTypes = {
     mode: PropTypes.string.isRequired,
     setMode: PropTypes.func.isRequired
-  }
+}
