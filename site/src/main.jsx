c6a120998d1cf2ee0fac901258f3671c717c776d
Benjamin Clark <clarkben@meta.com>
2024-05-15 16:05:12 -0400

    Remove dependency on vite-static-copy plugin, instead use the vite URL import functionality to init wasm.

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import initWasm from "@geoarrow/geoarrow-wasm/esm/index.js";
+import wasmUrl from "@geoarrow/geoarrow-wasm/esm/index_bg.wasm?url"
 
 //TODO: Make this async and parallelize with the startup of the map component, rather than blocking in. 
-await initWasm();
+await initWasm(wasmUrl);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- site/vite.config.js
+++ site/vite.config.js
@@ -1,27 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../../geoarrow-rs/js/pkg/esm/index_bg.wasm',
-          dest: 'assets'
-        },
-        {
-          src: '../../geoarrow-rs/js/pkg/esm/index_bg.wasm.d.ts',
-          dest: 'assets'
-        },
-        {
-          src: 'public/nybb.parquet',
-          dest: 'assets'
-        }
-      ]
-    })
   ],
   build: {
     target: 'esnext',
