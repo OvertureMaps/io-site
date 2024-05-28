7c4523772fa859a98276ad03c1a31a8bca51bd77
ethaniannicelli <eti7075@rit.edu>
2024-05-28 15:13:20 -0400

    Added startup modal with tour options

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -7,9 +7,25 @@ import { keepTheme } from "./themeUtils";
 import { useState, useEffect } from "react";
 import Joyride from "react-joyride";
 import Steps from "./Tour";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
 
 function App() {
   const [modeName, setModeName] = useState("theme-dark");
+  const [run, setRun] = useState(false);
+  const [tour, setTour] = useState(true);
+  const [open, setOpen] = useState(tour);
+
+  const startTour = () => {
+    setOpen(false);
+    setRun(true);
+  };
+
+  const endTour = () => {
+    setOpen(false);
+    setTour(false);
+  };
 
   useEffect(() => {
     keepTheme(setModeName);
@@ -17,7 +33,15 @@ function App() {
 
   return (
     <div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box sx={style}>
+          <Button onClick={startTour}>Start Tour</Button>
+          <Button onClick={() => setOpen(false)}>Skip Tour</Button>
+          <Button onClick={endTour}>Stop Tour</Button>
+        </Box>
+      </Modal>
       <Joyride
+        run={run}
         steps={Steps}
         continuous={true}
         showProgress={true}
@@ -34,3 +58,15 @@ function App() {
 }
 
 export default App;
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
