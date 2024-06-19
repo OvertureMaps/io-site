044952ba798af8e09211e94a1b6dfd8cd32aa984
ethaniannicelli <etiannicelli@gmail.com>
2024-06-19 12:48:09 -0400

    Styling for copy button

--- site/src/nav/CommandCopyButton.jsx
+++ site/src/nav/CommandCopyButton.jsx
@@ -26,10 +26,10 @@ function CommandCopyButton({ mode, visibleThemes }) {
   const handleClick = () => {
     const bounds = myMap.getBounds();
     let bbox = [
-      bounds.getWest(), //minx
-      bounds.getSouth(), //miny
-      bounds.getEast(), //maxx
-      bounds.getNorth(), //maxy
+      " " + bounds.getWest(), //minx
+      " " + bounds.getSouth(), //miny
+      " " + bounds.getEast(), //maxx
+      " " + bounds.getNorth(), //maxy
     ];
 
     let visibleTypes = [];
@@ -40,11 +40,10 @@ function CommandCopyButton({ mode, visibleThemes }) {
     let commandList = [];
     for (let i = 0; i < visibleTypes.length; i++) {
       commandList.push(
-        "overturemaps download --bbox " +
+        "overturemaps download --bbox" +
           bbox +
           " -f geojson --type " +
-          visibleTypes[i] +
-          " "
+          visibleTypes[i]
       );
     }
 
@@ -55,9 +54,28 @@ function CommandCopyButton({ mode, visibleThemes }) {
   return (
     <div className="copy-command-wrapper">
       <Floater
-        styles={{}}
+        styles={{
+          container: {
+            width: "520px",
+            marginLeft: "-200px",
+            borderRadius: "10px",
+            padding: "10px",
+            color: mode === "theme-dark" ? "white" : "black",
+            fontSize: "1rem",
+            background:
+              mode === "theme-dark"
+                ? "var(--ifm-navbar-background-color)"
+                : "var(--ifm-color-secondary-light)",
+          },
+          arrow: {
+            color:
+              mode === "theme-dark"
+                ? "var(--ifm-navbar-background-color)"
+                : "var(--ifm-color-secondary-light)",
+          },
+        }}
         content={
-          <div>
+          <div className="copy-container">
             Use these commands with the{" "}
             <a
               href={"https://github.com/OvertureMaps/overturemaps-py"}
@@ -67,7 +85,11 @@ function CommandCopyButton({ mode, visibleThemes }) {
               python downloader
             </a>{" "}
             to download the current layers:
-            <div className="commands">{commands}</div>
+            <div className="commands">
+              {commands.map((command) => (
+                <p>{command}</p>
+              ))}
+            </div>
           </div>
         }
         open={showFloater}
