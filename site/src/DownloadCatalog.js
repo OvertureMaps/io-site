aa5cea0eb0c970a0439ceb73d902f70253dfc3a8
Benjamin Clark <clarkben@meta.com>
2025-01-24 11:28:12 -0500

    Bump to january download catalog.

--- site/src/DownloadCatalog.js
+++ site/src/DownloadCatalog.js
@@ -1,4 +1,4 @@
-import manifest from './manifests/12-18-manifest.json';
+import manifest from './manifests/01-22-25-manifest.json';
 
 const awsbasepath = 'https://overturemaps-us-west-2.s3.amazonaws.com/release/';
 
